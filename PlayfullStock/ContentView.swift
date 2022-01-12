ed8ad9dbf19d6a81579f5c2cb14652425ad6d9bf
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-12 23:30:02 +0200

    Initial Commit

---
+++ PlayfullStock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayfullStock
+//
+//  Created by AbdelrahmanQasim on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayfullStock/PlayfullStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayfullStockApp.swift
+//  PlayfullStock
+//
+//  Created by AbdelrahmanQasim on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayfullStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
